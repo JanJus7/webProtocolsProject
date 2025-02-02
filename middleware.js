9e16cf13688f0b8b1a4851db159151c4fa764009
Jan <janjus2004@gmail.com>
2025-02-02 13:46:06 +0100

    deleted userId from middleware cus it works the same without it and generated errors

--- middleware.js
+++ middleware.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
 
 export async function middleware(request) {
   const pathname = request.nextUrl.pathname;
@@ -8,10 +7,9 @@ export async function middleware(request) {
     pathname.startsWith(route)
   );
 
-  const userId = (await cookies().get("userId"))?.value;
   const isGuestSession = pathname === "/game/guest-session";
 
-  if (isProtectedRoute && !userId && !isGuestSession) {
+  if (isProtectedRoute && !isGuestSession) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
