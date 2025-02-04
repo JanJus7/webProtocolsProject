69b5fb8ce38f01b726da775f2a892f54662b040c
Jan <janjus2004@gmail.com>
2025-02-04 22:49:33 +0100

    changed the coloru of text

--- app/game/websocket/page.js
+++ app/game/websocket/page.js
@@ -74,7 +74,7 @@ export default function WebSocketChat() {
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="flex-grow px-3 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-grow px-3 py-2 border border-gray-300 text-black rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             onClick={sendMessage}
